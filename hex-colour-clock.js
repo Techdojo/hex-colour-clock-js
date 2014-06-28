14230ab7a345aef03e85f7b4a28f5894949a4143
Tris <trisashley@gmail.com>
2014-06-28 14:05:58 +0100

    Reference comments added to script
    Renamed main script file

--- hex-colour-clock.js
+++ hex-colour-clock.js
@@ -1,15 +1,33 @@
 function Hex_Clock(config){
     "use strict";
 
-    var _config = {},
-        exports = {},
-        _body = document.getElementsByTagName('body')[0];
+    ////////////////////////////
+    //On Object Instantiation //
+    ////////////////////////////
 
+        //Internal Config Object
+    var _config = {},
+        //body node
+        _body = document.getElementsByTagName('body')[0],
+        //Object that the public method is attached to
+        exports = {};
+
+
+    //Settings values of internal config object depending on config values passed in
     _config = {
+        //Main Element
         element: config.element,
+
+        //Render time bool, defaults to true is not passed in
         render_time: ((config.render_time !== undefined) ? config.render_time : true ),
+
+        //clock custom element, defaults to false is no passed in
         clock_element: ((config.clock_element !== undefined) ? config.clock_element : false )
     };
+
+    ////////////////////
+    //Private Methods //
+    ////////////////////
 
     function _render_time(){
         var html = [],
@@ -19,6 +37,7 @@ function Hex_Clock(config){
             seconds = (date.getSeconds() <= 9) ? '0' + date.getSeconds() : date.getSeconds(),
             hex = '#' + hour + mins + seconds;
 
+        //Build HTML
         html.push('<div id="hex-clock-time" class="hex-clock-colour">');
         html.push('<span class="hex-symbol">#</span>');
         html.push('<span class="hex-section hex-hours">' + hour + '</span>');
@@ -26,44 +45,67 @@ function Hex_Clock(config){
         html.push('<span class="hex-section hex-seconds">' + seconds + '</span>');
         html.push('</div>');
 
+        //If render_time == true, add time html
         if(_config.render_time){
-            html.push(_get_clock_html(hour, mins, seconds));
+            html.push(_set_clock_html(hour, mins, seconds));
         }
 
+        //Join html array, add clock html to element
         _config.element.innerHTML = html.join('');
+
+        //Change body background colour to be that of the hex colour
         _body.style.backgroundColor = hex;
 
+        //timeout to call itself every 1 second
         setTimeout(_render_time, 1000);
     }
 
-    function _get_clock_html(hour, mins, seconds) {
+    function _set_clock_html(hour, mins, seconds) {
+        //Check if custom time element has been set
         if(_config.clock_element){
+            //If custom element has been set change it's html to the current time
             _config.clock_element.innerHTML = (hour + ':' + mins + ':' + seconds);
         } else {
+            //If custom element has NOT been set, return hex clock html to be added to html array
             return '<div id="hex-clock-colour" class="hex-clock-time">' + (hour + ':' + mins + ':' + seconds) + '</div>';
         }
     }
 
     function _add_class_to_wrap_element(){
+        //Add class to main element
         _config.element.className += _config.element.className ? ' hex-clock-wrap' : 'hex-clock-wrap';
     }
 
-    exports.init = function(){
-        console.log('internal config', _config);
+    /////////////////////
+    //Public Method(s) //
+    /////////////////////
 
+    exports.init = function(){
         _add_class_to_wrap_element();
         _render_time();
     };
 
+    //Return public methods object
     return exports;
 }
+
 
 document.addEventListener('DOMContentLoaded', function(){
     "use strict";
 
+    /////////////////////////////////////
+    //Instantiate New Hex_Clock object //
+    /////////////////////////////////////
+
     Hex_Clock({
-        element: document.getElementById('hex-clock-wrap')
-        // render_time: false
-        // clock_element: document.getElementById('actual-time')
+        // Element that the hex clock will be added to
+        element: document.getElementById('hex-clock-wrap'),
+
+        //On / Off Switch for displaying / hiding the time
+        render_time: true,
+
+        //Custom Element to render the clock in
+        //clock_element: document.getElementById('element-id')
     }).init();
+
 }, false);
